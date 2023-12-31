742dce1d7008a4a3819c7bf330300cc046723b1c
Abhishek Jethuri <abhishek.jethuri@gmail.com>
2023-12-31 14:00:15 +0530

    Add public directory to static in express

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
